0b412b407b86e522bc2c3d521d373f292b022b69
tanahiro2010 <herentongkegu087@gmail.com>
2025-06-07 19:41:32 +0900

    Update and add session handler and add ws

---
+++ src/wss/index.js
@@ -0,0 +1,9 @@
+const WebSocket = require('ws');
+const http = require('http');
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+
+});
