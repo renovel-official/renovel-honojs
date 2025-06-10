c38565a4d8f7f2e65693be64fad4bf5b3162736a
tanahiro2010 <herentongkegu087@gmail.com>
2025-06-10 17:01:30 +0900

    update

--- src/app/author/messages/create/index.tsx
+++ src/app/author/messages/create/index.tsx
@@ -6,9 +6,15 @@ export default function AuthorMessagesCreate() {
             <AuthorTitle>メッセージを作成</AuthorTitle>
 
             <div class="mt-5 w-full border rounded p-3">
-                <div class="text-gray-500 text-center">
-                    メッセージを作成するユーザーを選択してください
-                </div>
+                <form id="user-add-form">
+                    <div class="text-gray-500 text-center">
+                        メッセージを作成するユーザーのIDを入力してください
+                    </div>
+
+                    <div class="mt-5 text-center">
+                        <input type="text" class="w-1/2 border rounded p-2" placeholder="ユーザーID" id="user-id" />
+                    </div>
+                </form>
             </div>
         </main>
     );
