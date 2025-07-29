dbb86bf44c081b29b96b26cc561fab9ba4106103
tanahiro2010 <herentongkegu087@gmail.com>
2025-07-29 09:18:24 +0900

    [update] タイプ変更による本文エリアの表示設定変更実装

--- public/assets/service/author/works/create.js
+++ public/assets/service/author/works/create.js
@@ -1,2 +1,16 @@
 const titleEle = document.querySelector('#title');
 const phraseEle = document.querySelector('#phrase');
+const genreEle = document.querySelector('#genre');
+const tagsEle = document.querySelector('#tags');
+const descriptionEle = document.querySelector('#description');
+const typeEle = document.querySelector('#type');
+const contentAreaEle = document.querySelector('#text-section');
+
+typeEle.addEventListener('change', (e) => {
+    const type = e.target.value;
+    if (type === 'short') {
+        contentAreaEle.classList.remove('hidden');
+    } else {
+        contentAreaEle.classList.add('hidden');
+    }
+});
